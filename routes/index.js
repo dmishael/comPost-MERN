944fcc6ad610218f2b9ab660636de01b65982ce4
daniel mishael <dmishael91@gmail.com>
2019-01-25 19:14:24 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+// const creatureController = require('../controllers/creatureController')
+
+// router.get('/', creatureController.index)
+// router.get('/creatures/:id', creatureController.show)
+
+module.exports = router
